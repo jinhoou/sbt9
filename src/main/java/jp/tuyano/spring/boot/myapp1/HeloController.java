40229e75d9288526e58994bb2a8090709b903089
Jinho Oh <jinhoou@gmail.com>
2017-10-09 01:30:05 +0900

    add index.jsp

--- src/main/java/jp/tuyano/spring/boot/myapp1/HeloController.java
+++ src/main/java/jp/tuyano/spring/boot/myapp1/HeloController.java
@@ -1,15 +1,18 @@
 package jp.tuyano.spring.boot.myapp1;
 
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 
 public class HeloController
 {
     @RequestMapping("/")
     public String index()
     {
-        return ("hello, spring boot2");
+        return "index";
     }
 }
